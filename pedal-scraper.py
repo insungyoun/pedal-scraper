59922f285a92f952045aeb3e3ed9d1b7b586ca2a
insungyoun <insung.youn@mail.utoronto.ca>
2023-09-18 22:45:44 -0400

    Parsing step complete and more

--- pedal-scraper.py
+++ pedal-scraper.py
@@ -96,13 +96,26 @@ def modify_url(link: str, preference: str) -> None:
                         break
             break
 
-# Here is the code for the user input dialog, where we call 'modify_url' multiple times
+# Here is the code for the user input dialog, where we call 'modify_url' multiple times and provide CSV commands
 modify_url(url, "Search Term")
 modify_url(url, "Free 2-Day Shipping")
 modify_url(url, "Price Range")
 modify_url(url, "Handmade Only")
 modify_url(url, "Reverb Credits")
 modify_url(url, "Preferred Sellers")
+output_csv = False
+while True:
+    try:
+        csv_status = input("Output CSV file? [Y/N]\n")
+        if csv_status.replace(' ', '').lower() not in {'y', 'n'}:
+            raise InvalidInputException
+    except InvalidInputException:
+        print("Invalid input, please try again.\n")
+        continue
+    else:
+        if csv_status.replace(' ', '').lower() == 'y':
+            output_csv = True
+        break
 
 # Here, chromium headless browser is invoked (and downloaded upon first-time use) and scraping is performed
 session = HTMLSession()
@@ -110,23 +123,79 @@ response = session.get(url)
 response.html.render(sleep=1, keep_page=True, scrolldown=2) # sleep and scrolldown values may noy work, experiment further
 products = response.html.find('.grid-card')
 
-# Here, the data stored in 'products' will be parsed
-# The name of the pedal will be before the first newline character '\n'
-# The price of the pedal will be after the first newline character and before the second newline character
-# The condition of the pedal will be after the second newline character and before the third newline character
-for product in products:
-    pedal_info = product.text
-    pedal_link = product.absolute_links
+count = 0 # For testing purposes only (this value should be 60)
+pedal_data = []
+if output_csv is True:
+    # Here, the data stored in 'products' will be parsed
+    csv_file = ""
+    csv_header = "name,price,condition,country,shipping,link\n"
+    csv_observations = ""
+    for product in products:
+        csv_line = ""
+        text = product.text # product text that will be parsed
+        if text[:12] == "Reverb Bump\n": # this step removes "Reverb Bump\n" if it exists, because we don't need it
+            text = text[12:]
+        link_set = product.absolute_links # the set containing the link to the product
+        link = "" # the link to the product
+        for link_str in link_set: # extracting the link from the set with the link
+            link = link_str
+        csv_line_list = text.split('\n')
+
+        # cleaning up price values in csv_line_list
+        for token in csv_line_list:
+            if token[0] == '$':
+                if "% Off" in token or "% price drop" in token:
+                    csv_line_list.remove(token)
+                else:
+                    if ',' in token:
+                        new_token = token.replace(',', '')
+                        csv_line_list[csv_line_list.index(token)] = new_token
+
+        # adding to pedal_data
+        pedal_data.append(csv_line_list)
+
+        name, price, condition, country, shipping = csv_line_list[0], csv_line_list[1], csv_line_list[2], \
+            csv_line_list[3], "Paid"
+        if "Free Shipping" in csv_line_list or "Free 2-Day Shipping" in csv_line_list:
+            shipping = csv_line_list[4]
+        csv_line += name + ',' + price + ',' + condition + ',' + country + ',' + shipping + ',' + link + '\n'
+        csv_observations += csv_line
+        count += 1
+        print(text + '\n')
+    # Note: the last line of the CSV file is just a newline character (blank line)
+    csv_file = open("pedal-scraper.csv", "w")
+    csv_file.write(csv_header)
+    csv_file.write(csv_observations)
+    csv_file.close()
+    print(str(count) + " products scraped.\nThe outputted CSV file is in the same parent directory as this script.")
+else:
+    print(str(count) + " products scraped.\n")
+
+# This section of code writes the statistical summary of the pedals to stdout
+print(pedal_data)
 
 
-# Here, testing is done (will be removed when project completed)
-count = 0
-for product in products:
-    pedal = {
-        'name': product.text,
-        'link': product.absolute_links
-    }
-    count += 1
-    print(pedal)
-    print('\n')
-print(count)
+# The first line in pedal['name'] (the part before the first occurring '\n' character) is either 'Reverb Bump' or the
+# name of the pedal. This means that we must first check to see if the first line in 'Reverb Bump' or not. However, we
+# have no use for 'Reverb Bump' concerning our purposes, so we only check for the purposes of accurately extracting the
+# name of the pedal.
+# We need:
+    # name (always)
+    # price (always)
+        # '$xCAD'. We know that currency codes are always 3 characters long and the dollar sign is one character long
+    # condition (always)
+        # Brand New
+        # Used - B-Stock
+        # Used - Mint
+        # Used - Excellent
+        # Used - Very Good
+        # Used - Good
+        # Used - Fair
+        # Used - Poor
+        # Used - Non Functioning
+    # country (always)
+    # shipping (optional)
+        # Free 2-Day Shipping
+        # Free Shipping
+        # Paid
+    # link (always)
